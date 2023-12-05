c9baba7a37e08f68a4dc4df259826be63fd08ef2
vikaskumar1 <vikas.kumar@nwea.org>
2023-12-05 15:51:01 +0530

    add WebTestClient for http call test in PersonControllerTest

--- src/test/java/com/example/demoreactive/controller/PersonControllerTest.java
+++ src/test/java/com/example/demoreactive/controller/PersonControllerTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockitoAnnotations;
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
