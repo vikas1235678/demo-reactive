175689ab3037993343b130162467230d80a389ab
vikaskumar1 <vikas.kumar@nwea.org>
2023-12-07 15:07:12 +0530

    remove unused import

--- src/test/java/com/example/demoreactive/DemoReactiveApplicationTests.java
+++ src/test/java/com/example/demoreactive/DemoReactiveApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.demoreactive;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
